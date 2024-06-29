3c1e39b74aeff0da9748f502b63de9482c7bbb6b
andredw13L <andreribeiro13l@gmail.com>
2024-06-29 18:34:19 -0300

    Conectando com API pelo React

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,6 +6,13 @@ function Select({ text, name, options, handleOnChange, value }) {
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name} defaultValue={'Default'}>
                 <option disabled value="Default">Selecione uma opção</option>
+                {options.map((option) => (
+                    <option
+                        value={option.id}
+                        key={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     )
